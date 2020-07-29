b68a9c3d8fa659d55bc972d307f7a8530c32f4d5
SalammaIlari <51205520+SalammaIlari@users.noreply.github.com>
2020-07-29 11:16:32 +0530

    Add files via upload

---
+++ sulight.py
@@ -0,0 +1,12 @@
+#Calculate the surface of the building exposed to sunlight 
+def Sunlight(arr, n): 
+    count = 1 
+    curr_max = arr[0] 
+    for i in range(1, n): 
+        if (arr[i] > curr_max): 
+            count += 1
+            curr_max = arr[i] 
+    return count 
+arr = list(map(int,input().split()))
+n = len(arr) 
+print(Sunlight(arr,n)) 
